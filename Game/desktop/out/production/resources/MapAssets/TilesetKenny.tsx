d9da1f4801793ffc8e6099b3902ffb9d7a4d698f
Adham125 <94682626+Adham125@users.noreply.github.com>
2022-02-01 17:45:58 +0000

    added executable jar file that works

--- Game/desktop/out/production/resources/MapAssets/TilesetKenny.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="TilesetKenny" tilewidth="32" tileheight="32" tilecount="384" columns="32">
- <image source="KennyPirate/Tilesheet/tiles_sheet.png" width="1024" height="384"/>
-</tileset>
